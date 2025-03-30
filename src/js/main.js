ce55d490134b39bb497587c0a895a98189dfa435
ricsi <ricsi@ricsis-MacBook-Pro>
2025-03-30 19:17:10 +0200

    x-lesson-11_Download-elems

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
 import Difference from './modules/difference';
 import Form from './modules/forms';
+import ShowInfo from './modules/showInfo';
+import Download from './modules/download';
 
 window.addEventListener('DOMContentLoaded', () => {
   const slider = new MainSlider({btns: '.next', container: '.page'});
@@ -45,4 +47,8 @@ window.addEventListener('DOMContentLoaded', () => {
   difference.init();
 
   new Form('.form').init();
+
+  new ShowInfo('.plus__content').init();
+
+  new Download('.download').init();
 }); 
